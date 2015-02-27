d4e626368167a11f742117e21c29aa047201d74e
Alen Buhanec <alen.buhanec@gmail.com>
2015-02-27 18:13:24 +0000

    update 4 mac

--- coursework.c
+++ coursework.c
@@ -54,70 +54,37 @@ void compute() {
         __m128 y1_ = _mm_load1_ps(y+i);
         __m128 z1_ = _mm_load1_ps(z+i);
         for (j = 0; j < V; j += 4) {
-            //float rx = x[j] - x[i];
-            //float rx1 = x[j+1] - x[i];
-            //float rx2 = x[j+2] - x[i];
-            //float rx3 = x[j+3] - x[i];
             __m128 x_ = _mm_load_ps(x+j);
             __m128 rx_ = _mm_sub_ps(x_, x1_);
-            //float ry = y[j] - y[i];
-            //float ry1 = y[j+1] - y[i];
-            //float ry2 = y[j+2] - y[i];
-            //float ry3 = y[j+3] - y[i];
             __m128 y_ = _mm_load_ps(y+j);
             __m128 ry_ = _mm_sub_ps(y_, y1_);
-            //float rz = z[j] - z[i];
-            //float rz1 = z[j+1] - z[i];
-            //float rz2 = z[j+2] - z[i];
-            //float rz3 = z[j+3] - z[i];
             __m128 z_ = _mm_load_ps(z+j);
             __m128 rz_ = _mm_sub_ps(z_, z1_);
-            //float r2 = rx*rx + ry*ry + rz*rz + eps;
-            //float r21 = rx1*rx1 + ry1*ry1 + rz1*rz1 + eps;
-            //float r22 = rx2*rx2 + ry2*ry2 + rz2*rz2 + eps;
-            //float r23 = rx3*rx3 + ry3*ry3 + rz3*rz3 + eps;
             __m128 x2_ = _mm_mul_ps(rx_, rx_);
             __m128 y2_ = _mm_mul_ps(ry_, ry_);
             __m128 z2_ = _mm_mul_ps(rz_, rz_);
             __m128 r2_ = _mm_add_ps(_mm_add_ps(_mm_add_ps(x2_, y2_), z2_), eps_);
-            //float r2inv = 1.0f / sqrt(r2);
-            //float r2inv1 = 1.0f / sqrt(r21);
-            //float r2inv2 = 1.0f / sqrt(r22);
-            //float r2inv3 = 1.0f / sqrt(r23);
-            __m128 r2inv_ = _mm_div_ps(one_, _mm_sqrt_ps(r2_));
+            // Newton-Phelps + fast inverse
+            //*
+            __m128 inv_ = _mm_rsqrt_ps(r2_);
+            __m128 top_ = _mm_mul_ps(_mm_mul_ps(r2_, inv_), inv_);
+            __m128 r2inv_ = _mm_mul_ps(_mm_mul_ps(half_, inv_), _mm_sub_ps(three_, top_));
+            //*/
+            // Accurate inverse square root
+            //__m128 r2inv_ =_mm_div_ps(one_, _mm_sqrt_ps(r2_));
+            // Magic bullshit
             //__m128 r2inv_ = _mm_rsqrt_ps(r2_);
-
-            //float r6inv = r2inv * r2inv * r2inv;
-            //float r6inv1 = r2inv1 * r2inv1 * r2inv1;
-            //float r6inv2 = r2inv2 * r2inv2 * r2inv2;
-            //float r6inv3 = r2inv3 * r2inv3 * r2inv3;
             __m128 r6inv_ = _mm_mul_ps(_mm_mul_ps(r2inv_, r2inv_), r2inv_);
-            //float s = m[j] * r6inv;
-            //float s1 = m[j+1] * r6inv1;
-            //float s2 = m[j+2] * r6inv2;
-            //float s3 = m[j+3] * r6inv3;
             __m128 m_ = _mm_load_ps(m+j);
             __m128 s_ = _mm_mul_ps(m_, r6inv_);
-            //ax[i] += s * rx;
-            //ax[i] += s1 * rx1;
-            //ax[i] += s2 * rx2;
-            //ax[i] += s3 * rx3;
             __m128 mx_ = _mm_mul_ps(s_, rx_);
             __m128 sx_ = _mm_hsum_ps(mx_);
             __m128 ax_ = _mm_load1_ps(ax+i);
             _mm_store_ss(ax+i, _mm_add_ss(ax_, sx_));
-            //ay[i] += s * ry;
-            //ay[i] += s1 * ry1;
-            //ay[i] += s2 * ry2;
-            //ay[i] += s3 * ry3;
             __m128 my_ = _mm_mul_ps(s_, ry_);
             __m128 sy_ = _mm_hsum_ps(my_);
             __m128 ay_ = _mm_load1_ps(ay+i);
             _mm_store_ss(ay+i, _mm_add_ss(ay_, sy_));
-            //az[i] += s * rz;
-            //az[i] += s1 * rz1;
-            //az[i] += s2 * rz2;
-            //az[i] += s3 * rz3;
             __m128 mz_ = _mm_mul_ps(s_, rz_);
             __m128 sz_ = _mm_hsum_ps(mz_);
             __m128 az_ = _mm_load1_ps(az+i);

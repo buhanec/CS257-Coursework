3e664bb14ff62933c981bf9fd1ce991b477eee9b
Alen Buhanec <alen.buhanec@gmail.com>
2015-03-09 13:29:00 +0000

    broken but merged

---
+++ loop0.c
@@ -0,0 +1,20 @@
+    // Loop 0.
+    /*
+    t0 = wtime();
+    for (i = 0; i < V; i += 4) {
+        _mm_store_ps(ax+i, zero_);
+    }
+    for (i = 0; i < V; i += 4) {
+        _mm_store_ps(ay+i, zero_);
+    }
+    for (i = 0; i < V; i += 4) {
+        _mm_store_ps(az+i, zero_);
+    }
+    for (; i < N; i++) {
+        _mm_store_ss(ax+i, zero_);
+        _mm_store_ss(ay+i, zero_);
+        _mm_store_ss(az+i, zero_);
+    }
+    t1 = wtime();
+    l0 += (t1 - t0);
+    */
